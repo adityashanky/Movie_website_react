232e9013fd1b7da3ad36daddfd971ffd7a985436
Aditya Singh <adityasingh57927@gmail.com>
2025-12-24 16:44:11 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import './css/App.css';
+import Favourites from './pages/Favorites';
+import Home from './pages/Home';
+import {Routes, Route} from "react-router-dom";
+import { MovieProvider } from './context/MovieContext';
+import NavBar from "./components/NavBar";
+
+
+function App() { 
+  return (
+    <MovieProvider>
+      <NavBar />
+    <main className = " main-content" >
+      <Routes>
+        <Route path = "/" element={<Home />}/>
+        <Route path = "/favorites" element={<favorites />} />
+        </Routes>
+    </main>
+    </MovieProvider>
+  ); 
+}
+
+export default App;
